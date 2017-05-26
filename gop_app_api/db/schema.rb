a2e7962bb1caebb5f2f5b3ebca1ad8f19c004559
524855 <annguyen@C02S7148FVH8.home>
2017-05-26 00:59:35 -0400

    API partially commplete setup

--- gop_app_api/db/schema.rb
+++ gop_app_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170526005251) do
+ActiveRecord::Schema.define(version: 20170526011750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,9 +19,19 @@ ActiveRecord::Schema.define(version: 20170526005251) do
     t.string   "title"
     t.string   "url"
     t.string   "data_type"
-    t.integer  "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "comment"
+    t.string   "feature_url"
+    t.integer  "user_id"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "is_shared",   default: false
   end
 
   create_table "users", force: :cascade do |t|
