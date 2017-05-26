783d37dd5610da93e7f29bf79dc7b0bccd5f14e5
524855 <annguyen@C02S7148FVH8.home>
2017-05-25 21:02:01 -0400

    created features table

--- gop_app_api/db/schema.rb
+++ gop_app_api/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525200816) do
+ActiveRecord::Schema.define(version: 20170526005251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "features", force: :cascade do |t|
+    t.string   "title"
+    t.string   "url"
+    t.string   "data_type"
+    t.integer  "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
