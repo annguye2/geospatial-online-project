c1ebecc47c19a66ef662c3c28e2c13992f5e87ec
524855 <annguyen@C02S7148FVH8.home>
2017-05-29 11:10:18 -0400

    reset database after remove is_share col and add share col

--- gop_app_api/db/schema.rb
+++ gop_app_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170526220559) do
+ActiveRecord::Schema.define(version: 20170529150119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,9 +40,9 @@ ActiveRecord::Schema.define(version: 20170526220559) do
     t.string   "comment"
     t.string   "feature_url"
     t.integer  "user_id"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.boolean  "is_shared",   default: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.boolean  "shared"
   end
 
   create_table "users", force: :cascade do |t|
