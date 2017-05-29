e6bcef64f507287fde86710cf1d194091f05824e
524855 <annguyen@C02S7148FVH8.home>
2017-05-29 12:57:49 -0400

    add shareprojects table

--- gop_app_api/db/schema.rb
+++ gop_app_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529150119) do
+ActiveRecord::Schema.define(version: 20170529164918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,17 @@ ActiveRecord::Schema.define(version: 20170529150119) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.boolean  "shared"
+    t.integer  "notice"
+  end
+
+  create_table "shareprojects", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "comment"
+    t.string   "feature_url"
+    t.string   "creator"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
